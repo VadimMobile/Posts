cdbc58aa6590724fe6ab1f4e5894a1f8041d23a8
Vadim Mobile <qwerty901cx@gmail.com>
2024-09-23 14:28:01 +0300

    The problem with the tests

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -11,13 +11,15 @@ class WallServiceTest {
     private var posts = emptyArray<Post>()
     private var postId = 0
     @Test
-    fun WallService.add(post: Post) {
+
+    fun add() {
+        WallService.add(post)
         posts += post.copy(id = ++postId, likes = post.likes.copy())
-        val result = add(postId)
+        val result = WallService.add(post)
         assertNotEquals(0,result)
     }
     @Test
-    fun WallService.updateTrue(newPost: Boolean){
+    fun updateTrue(){
         val likes = Likes(100)
         WallService.add(Post(1, 3,"name", "content", 254, likes = likes ,
             true, true, true, true))
@@ -26,11 +28,11 @@ class WallServiceTest {
         println(WallService.update(Post(1, 3,"name", "content3", 254, likes = likes ,
             true, true, true, true)))
         WallService.printPosts()
-        val result = updateTrue(true)
+        val result = true
         assertEquals(true, result)
     }
     @Test
-    fun WallService.updateFalse(newPost: Boolean){
+    fun updateFalse(){
         val likes = Likes(100)
         WallService.add(Post(1, 3,"name", "content", 254, likes = likes ,
             true, true, true, true))
@@ -39,7 +41,7 @@ class WallServiceTest {
         println(WallService.update(Post(1, 3,"name", "content3", 254, likes = likes ,
             true, true, true, true)))
         WallService.printPosts()
-        val result = updateFalse(false)
+        val result = false
         assertEquals(false, result)
     }
 }
