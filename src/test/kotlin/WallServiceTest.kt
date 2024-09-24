6394204fa07ecda022efa4c563b00e7de8c1abca
Vadim Mobile <qwerty901cx@gmail.com>
2024-09-24 12:31:48 +0300

    The problem with the tests

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -8,14 +8,15 @@ class WallServiceTest {
     fun clearBeforeTest() {
         WallService.clear()
     }
-    private var posts = emptyArray<Post>()
     private var postId = 0
     @Test
 
     fun add() {
+        val likes = Likes(100)
+        val post = Post(0, 3,"name", "content", 254, likes = likes ,
+            true, true, true, true)
         WallService.add(post)
-        posts += post.copy(id = ++postId, likes = post.likes.copy())
-        val result = WallService.add(post)
+        val result = postId
         assertNotEquals(0,result)
     }
     @Test
@@ -23,7 +24,7 @@ class WallServiceTest {
         val likes = Likes(100)
         WallService.add(Post(1, 3,"name", "content", 254, likes = likes ,
             true, true, true, true))
-        WallService.add(Post(1, 3,"name", "content2", 254, likes = likes ,
+        WallService.add(Post(2, 3,"name", "content2", 254, likes = likes ,
             true, true, true, true))
         println(WallService.update(Post(1, 3,"name", "content3", 254, likes = likes ,
             true, true, true, true)))
@@ -36,9 +37,9 @@ class WallServiceTest {
         val likes = Likes(100)
         WallService.add(Post(1, 3,"name", "content", 254, likes = likes ,
             true, true, true, true))
-        WallService.add(Post(1, 3,"name", "content2", 254, likes = likes ,
+        WallService.add(Post(2, 3,"name", "content2", 254, likes = likes ,
             true, true, true, true))
-        println(WallService.update(Post(1, 3,"name", "content3", 254, likes = likes ,
+        println(WallService.update(Post(3, 3,"name", "content3", 254, likes = likes ,
             true, true, true, true)))
         WallService.printPosts()
         val result = false
